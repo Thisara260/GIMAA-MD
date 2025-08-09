32ef34d99cd18ce4861b03aaae5778a766c259b9
Thisara260 <thisaragimhana011@gmail.com>
2025-08-09 15:52:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
   OWNER_NUM: process.env.OWNER_NUM || "94704198014",
   PREFIX: process.env.PREFIX || ".", 
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qxg4wQP/shaban-md.jpg",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n> > 𝛲𝛩𝑊𝛯𝑅𝐷 𝐵𝑌 ＧIᗰ𝛥𝛥",
+  MODE: process.env.MODE || "public"
 };
