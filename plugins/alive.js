b80c98f0c04baa9974b62f7fd3a2c1f0df33f5d1
Thisara260 <thisaragimhana011@gmail.com>
2025-08-10 12:52:14 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,6 +13,7 @@ async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sende
 try{
     await robin.sendPresenceUpdate('recording', from);
     await robin.sendMessage(from, { audio: { url: "https://github.com/ROBIN-MAX-YT/BOT-HELPER/raw/refs/heads/main/audio/Owner.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+    await robin.sendMessage(from,{sticker: { url : "https://th.bing.com/th/id/R.e69b65dabca591c4bbe140a32f520682?rik=g48oRqfhchLIPA&pid=ImgRaw&r=0" },package: ' 𝛲𝛩𝑊𝛯𝑅𝐷 𝐵𝑌 ＧIᗰ𝛥𝛥'},{ quoted: mek })
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
