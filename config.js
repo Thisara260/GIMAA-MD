e6f898ec1dcc9c9d8bb89be7d27043eceac75e50
Thisara260 <thisaragimhana011@gmail.com>
2025-08-13 17:06:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "avYT2YbZ#ukCAwFFF5AZv7hm45iYPvScJdnSqY2iHcFaJgXAy6Ss",
   OWNER_NUM: process.env.OWNER_NUM || "94704198014",
   PREFIX: process.env.PREFIX || ".", 
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qxg4wQP/shaban-md.jpg",
