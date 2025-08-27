7369d5326b756c255be925f5bfd181c1dd3f8072
Thisara260 <thisaragimhana011@gmail.com>
2025-08-27 19:40:56 +0530

    Add files via upload

---
+++ settingss.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  SESSION_ID: "",  // add your Session Id here
+  OWNER_NUMBER: "94704198014", // put your phone number here
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  TIMEZONE: "Sri Lanka/Colombo" //put your country timeZone....leave blank if u don't know.
+};
